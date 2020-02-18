22be57e504c7f95d202c4c9c692e1dd11210c12c
Fepe Marte <felipempduarte@outlook.com>
2020-02-18 16:22:42 -0300

    Inserido validacao de campos

--- CadastroDePacientesBe3/Controllers/ClientesController.cs
+++ CadastroDePacientesBe3/Controllers/ClientesController.cs
@@ -14,9 +14,68 @@ namespace CadastroDePacientesBe3.Controllers
         {
             _context = context;
         }
+     
+        [HttpGet]
         public IActionResult Create()
+        {
+            ViewBag.States = GetStates();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Clientes Cliente)
+        {
+            return View(Cliente);
+        }
+
+        [HttpGet]
+        public IActionResult List()
         {
             return View();
         }
+
+
+
+
+
+
+
+
+        public List<string> GetStates()
+        {
+            List<string> states = new List<string>();
+
+            states.Add("AC");
+            states.Add("AL");
+            states.Add("AP");
+            states.Add("AM");
+            states.Add("BA");
+            states.Add("CE");
+            states.Add("DF");
+            states.Add("ES");
+            states.Add("GO");
+            states.Add("MA");
+            states.Add("MT");
+            states.Add("MS");
+            states.Add("MG");
+            states.Add("PA");
+            states.Add("PB");
+            states.Add("PR");
+            states.Add("PE");
+            states.Add("PI");
+            states.Add("RJ");
+            states.Add("RN");
+            states.Add("RS");
+            states.Add("RO");
+            states.Add("RR");
+            states.Add("SC");
+            states.Add("SP");
+            states.Add("SE");
+            states.Add("TO");
+
+            return states;
+        }
+
+
     }
 }
--- CadastroDePacientesBe3/Models/Clientes.cs
+++ CadastroDePacientesBe3/Models/Clientes.cs
@@ -1,22 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CadastroDePacientesBe3.Models
 {
     public partial class Clientes
-    {
+    {       
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public int Prontuario { get; set; }
+
+        [StringLength(60, ErrorMessage = "Digite menos caracteres!")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Nome { get; set; }
+
+        [StringLength(80, ErrorMessage = "Digite menos caracteres!")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public DateTime DtNasc { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Rg { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Cpf { get; set; }
         public byte Sexo { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string FoneRes { get; set; }
+
+        [EmailAddress(ErrorMessage = "Digite um email valido!")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public short? IdConvenio { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string NCarteirinha { get; set; }
         public string Celular { get; set; }
+
+        [StringLength(100, ErrorMessage = "Digite menos caracteres!")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string NomeMae { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        public short? IdUF { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        public DateTime DtValidade { get; set; }
     }
 }
